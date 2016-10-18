b2f307c0bd659cbc6e0bcf3db019ddd1bcb4ca0a
Denis Matveev <mast747@bk.ru>
2016-10-19 01:39:34 +0300

    Add AddForm component

---
+++ src/js/actions/AppActions.js
@@ -0,0 +1,10 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var AppConstants = require('../constants/AppConstants');
+
+var AppActions = {
+  saveVideo: function(video) {
+
+  }
+}
+
+module.exports = AppActions;
